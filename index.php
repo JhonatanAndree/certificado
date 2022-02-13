c497ea987bf258093da25532be103ff1e971d812
JhonatanAndree <jcandree@gmail.com>
2022-02-13 10:36:41 -0500

    Uso de template Bracket 2.0 y maquetación del Login

--- index.php
+++ index.php
@@ -1,1 +1,64 @@
-<h1>Test certificados y diplomas</h1>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Twitter -->
+    <meta name="twitter:site" content="@themepixels">
+    <meta name="twitter:creator" content="@themepixels">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Bracket">
+    <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="twitter:image" content="http://themepixels.me/bracket/img/bracket-social.png">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="http://themepixels.me/bracket">
+    <meta property="og:title" content="Bracket">
+    <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
+
+    <meta property="og:image" content="http://themepixels.me/bracket/img/bracket-social.png">
+    <meta property="og:image:secure_url" content="http://themepixels.me/bracket/img/bracket-social.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="600">
+
+    <!-- Meta -->
+    <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="author" content="ThemePixels">
+
+    <title>Iniciar Sesión</title>
+
+    <link href="/public/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="/public/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+
+    <!-- Bracket CSS -->
+    <link rel="stylesheet" href="/public/css/bracket.css">
+  </head>
+
+  <body>
+
+    <div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
+
+      <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white rounded shadow-base">
+        <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> EMPRESA <span class="tx-normal">]</span></div>
+        <div class="tx-center mg-b-30">Certificados y Diplomas</div>
+
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Ingresa tu correo electrónico">
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Ingresa tu contraseña">
+        </div>
+        <button type="submit" class="btn btn-info btn-block">Acceder</button>
+        <!-- El registro lo otorga el usuario administrador -->
+      </div>
+    </div>
+
+    <script src="/public/lib/jquery/jquery.js"></script>
+    <script src="/public/lib/popper.js/popper.js"></script>
+    <script src="/public/lib/bootstrap/bootstrap.js"></script>
+
+  </body>
+</html>
