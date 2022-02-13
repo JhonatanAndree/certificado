830f83d2bdf68e69e4771fadb86be2a4fdf2bfff
JhonatanAndree <jcandree@gmail.com>
2022-02-13 18:44:49 -0500

    Maquetación primaria del template de las vistas principales de Usuario dentro de la carpeta View.

---
+++ view/html/MainMenu.php
@@ -0,0 +1,31 @@
+<div class="br-logo"><a href="../UsuHome/"><span>[</span>Empresa<span>]</span></a></div>
+
+    <div class="br-sideleft overflow-y-auto">
+      <label class="sidebar-label pd-x-15 mg-t-20">Menú</label>
+      <div class="br-sideleft-menu">
+        <a href="../UsuHome/" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+            <span class="menu-item-label">Inicio</span>
+          </div><!-- menu-item -->
+        </a>
+        <a href="../UsuCurso/" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-ios-bookmarks-outline tx-24"></i>
+            <span class="menu-item-label">Mis Cursos</span>
+          </div><!-- menu-item -->
+        </a>
+        <a href="../UsuPerfil/" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-ios-gear-outline tx-20"></i>
+            <span class="menu-item-label">Perfil</span>
+          </div><!-- menu-item -->
+        </a>
+        <a href="#" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-power tx-18"></i>
+            <span class="menu-item-label">Cerrar Sesión</span>
+          </div><!-- menu-item -->
+        </a>
+      </div>
+    </div>
