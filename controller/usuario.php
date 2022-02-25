9b26f07c16c965cb25ee85b29ae1d6db33913bd8
JhonatanAndree <jcandree@gmail.com>
2022-02-24 21:30:47 -0500

    Codificación del Controlador y diseño de la tabla lista de cursos asignados a un usuario

---
+++ controller/usuario.php
@@ -0,0 +1,35 @@
+<?php
+    /* Llamando a la cadena de conexión. */
+    require_once('../config/conexion.php');
+    /* Llamando a la clase usuario */
+    require_once('../models/Usuario.php');
+    /* Inicializamos clase */
+    $usuario = new Usuario();
+
+    /* Opción de solicitud de controller */
+    switch($_GET['op']){
+        /* Microservicios para poder mostrar el listado de cursos de usuario con certificado. */
+        case 'listar_cursos':
+            $datos = $usuario->get_cursos_x_usuario($_POST['usu_id']);
+            $data = array();
+            /* Creación de tabla */
+            foreach($datos as $row){
+                $sub_array = array();
+                $sub_array[] = $row['cur_nom'];
+                $sub_array[] = $row['cur_fechini'];
+                $sub_array[] = $row['cur_fechfin'];
+                $sub_array[] = $row['inst_nom']." ".$row['inst_apep'];
+                $sub_array[] = '<button type="button" onclick="Certificado('.$row['cur_id'].');" id="'.$row["cur_id"].'" class="btn btn-outline-warning btn-icon"><div><i class="fa fa-edit"></i></div></button>';
+                $data[] = $sub_array;
+            }
+            /* Json */
+            $results = array(
+                "sEcho" => 1,
+                "iTotalRecords" => count($data),
+                "iTotalDisplayRecords" => count($data),
+                "aaData" => $data);
+            echo json_encode($results);
+
+            break;
+    }
+?>
--- models/Usuario.php
+++ models/Usuario.php
@@ -41,30 +41,29 @@
             $conectar=parent::conexion();
             parent::set_names();
             $sql="SELECT
-            td_curso_usuario.cur_id,
-            tm_curso.cur_id,
-            tm_curso.cur_nom,
-            tm_curso.cur_descrip,
-            tm_curso.cur_fechIni,
-            tm_curso.cur_fechFin,
-            tm_usuario.usu_id,
-            tm_usuario.usu_nom,
-            tm_usuario.usu_apep,
-            tm_usuario.usu_apem,
-            tm_instructor.inst_id,
-            tm_instructor.inst_nom,
-            tm_instructor.inst_apep,
-            tm_instructor.inst_apem
-            FROM `td_curso_usuario` INNER JOIN
-            tm_curso on td_curso_usuario.cur_id = tm_curso.cur_id INNER JOIN
-            tm_usuario ON td_curso_usuario.usu_id = tm_usuario.usu_id INNER JOIN
-            tm_instructor ON tm_curso.inst_id = tm_instructor.inst_id
-            WHERE
-            td_curso_usuario.usu_id = ?";
-        $sql=$conectar->prepare($sql);
-        $sql->bindValue(1, $usu_id);
-        $sql->execute();
-        return $resultado=$sql->fetchALL();
+                td_curso_usuario.cur_id,
+                tm_curso.cur_id,
+                tm_curso.cur_nom,
+                tm_curso.cur_descrip,
+                tm_curso.cur_fechIni,
+                tm_curso.cur_fechFin,
+                tm_usuario.usu_id,
+                tm_usuario.usu_nom,
+                tm_usuario.usu_apep,
+                tm_usuario.usu_apem,
+                tm_instructor.inst_id,
+                tm_instructor.inst_nom,
+                tm_instructor.inst_apep,
+                tm_instructor.inst_apem
+                FROM `td_curso_usuario` INNER JOIN
+                tm_curso on td_curso_usuario.cur_id = tm_curso.cur_id INNER JOIN
+                tm_usuario ON td_curso_usuario.usu_id = tm_usuario.usu_id INNER JOIN
+                tm_instructor ON tm_curso.inst_id = tm_instructor.inst_id
+                WHERE
+                td_curso_usuario.usu_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $usu_id);
+            $sql->execute();
+            return $resultado=$sql->fetchALL();
         }
     }
-?>
