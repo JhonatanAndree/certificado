e81abe0c6d07343bc24afff2acb1eb414d0eba0c
JhonatanAndree <jcandree@gmail.com>
2022-03-20 02:01:55 -0500

    Se ha utilizado la herramienta CANVAS para integrar el certificado con texto personalizado por el usuario (datos propios) y puedan aparecer en el diploma o certificado, se programó en certificado.js las varibles que permiten la integración de datos visuales en el certificado.

--- view/Certificado/certificado.js
+++ view/Certificado/certificado.js
@@ -1,11 +1,34 @@
-$(document).ready(function(){
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+/* Iniciamos la imagen */
+const image = new Image();
+/* Ruta de la imagen */
+image.src = "../../public/certificado.png";
+/* Dimensión y selección de imagen */
+ctx.drawImage(image, 0, 0, canvas.width, canvas.height); /* Dibujo de imagen. */
+/* Tamaño de la fuente */
+ctx.font = 'bold 45px Arial';
+/* Centrar texto superior e inferior */
+ctx.textAlign = 'center';
+ctx.textBaseline = 'middle';
+/* Definir ancho de imagen y lo dividimos entre 2 para colocarlo a la mitad*/
+var x = canvas.width / 2;
+/* Marcamos la posición en x y en y */
+ctx.fillText("Jhonatan", x, 300);
+ctx.fillText("Jhonatan2", x, 230);
+$(document).ready(function () {
     var curd_id = getUrlParameter('curd_id');
 
-    $.post("../../controller/usuario.php?op=mostrar_curso_detalle", {curd_id : curd_id}, function(data){
+    $.post("../../controller/usuario.php?op=mostrar_curso_detalle", { curd_id: curd_id }, function (data) {
         data = JSON.parse(data);
-        console.log(data);
+        /*         console.log(data); */
         $("#cur_descrip").html(data.cur_descrip);
     });
+
+    /* Otorgamos los datos necesarios para que pueda imprimir la imagen. */
+
+
 });
 
 var getUrlParameter = function getUrlParameter(sParam) {
--- view/Certificado/index.php
+++ view/Certificado/index.php
@@ -2,29 +2,30 @@
 <html lang="es" class="pos-relative">
 
 <head>
-  <?php include '../../view/html/MainHead.php'; ?>
-  <title>Certificado</title>
+    <?php include '../../view/html/MainHead.php'; ?>
+    <title>Certificado</title>
 </head>
 
 <body class="pos-relative">
-  <div class="ht-100v d-flex align-items-center justify-content-center">
-    <div class="wd-lg-70p wd-xl-50p tx-center pd-x-40">
-      <h1 class="tx-100 tx-xs-140 tx-normal tx-inverse tx-roboto mg-b-0">
-        <img src="../../public/certificado.png" class="img-fluid" alt="Responsive image">
-      </h1>
-      <br>
-      <p class="tx-16 mg-b-30 text-justify" id="cur_descrip"> <!-- Usamos un id para llamar a la descripción del curso (certificado.js) -->
-        
-      </p>
-      <div class="form-layout-footer">
-        <button class="btn btn-outline-info"><i class="fa fa-send mg-r-10"></i>PDF</button>
-        <button class="btn btn-outline-success"><i class="fa fa-send mg-r-10"></i>PNG</button>
-      </div>
+    <div class="ht-100v d-flex align-items-center justify-content-center">
+        <div class="wd-lg-70p wd-xl-50p tx-center pd-x-40">
+            <h1 class="tx-100 tx-xs-140 tx-normal tx-inverse tx-roboto mg-b-0">
+                <canvas id="canvas" width="900" height="600" class="img-fluid" alt="Responsive image"></canvas>
+                <!-- <img src="../../public/certificado.png" class="img-fluid" alt="Responsive image"> -->
+            </h1>
+            <br>
+            <p class="tx-16 mg-b-30 text-justify" id="cur_descrip">
+                <!-- Usamos un id para llamar a la descripción del curso (certificado.js) -->
+            </p>
+            <div class="form-layout-footer">
+                <button class="btn btn-outline-info"><i class="fa fa-send mg-r-10"></i>PDF</button>
+                <button class="btn btn-outline-success"><i class="fa fa-send mg-r-10"></i>PNG</button>
+            </div>
+        </div>
     </div>
-  </div>
 
-  <?php include '../html/MainJs.php'; ?>
-  <script type="text/javascript" src="certificado.js"></script>
+    <?php include '../html/MainJs.php'; ?>
+    <script type="text/javascript" src="certificado.js"></script>
 </body>
 
 </html>
