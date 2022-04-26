8676f89e21529576aa375bd04da56bb916bbb182
JhonatanAndree <jcandree@gmail.com>
2022-04-26 01:35:43 -0500

    Configuraciones iniciales de certificado y personalización

--- view/Certificado/certificado.js
+++ view/Certificado/certificado.js
@@ -1,33 +1,38 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-/* Iniciamos la imagen */
-const image = new Image();
-/* Ruta de la imagen */
-image.src = "../../public/certificado.png";
-/* Dimensión y selección de imagen */
-ctx.drawImage(image, 0, 0, canvas.width, canvas.height); /* Dibujo de imagen. */
-/* Tamaño de la fuente */
-ctx.font = 'bold 45px Arial';
-/* Centrar texto superior e inferior */
-ctx.textAlign = 'center';
-ctx.textBaseline = 'middle';
-/* Definir ancho de imagen y lo dividimos entre 2 para colocarlo a la mitad*/
-var x = canvas.width / 2;
-/* Marcamos la posición en x y en y */
-ctx.fillText("Jhonatan", x, 300);
-ctx.fillText("Jhonatan2", x, 230);
 $(document).ready(function () {
     var curd_id = getUrlParameter('curd_id');
 
     $.post("../../controller/usuario.php?op=mostrar_curso_detalle", { curd_id: curd_id }, function (data) {
         data = JSON.parse(data);
-        /*         console.log(data); */
         $("#cur_descrip").html(data.cur_descrip);
+        /* Iniciamos la imagen */
+        const image = new Image();
+        /* Ruta de la imagen */
+        image.src = "../../public/CertificadoOficial.png";
+        /* Dimensión y selección de imagen */
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height); /* Dibujo de imagen. */
+        /* Tamaño de la fuente */
+        ctx.font = 'bold 40px Arial';
+        /* Centrar texto superior e inferior */
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        /* Definir ancho de imagen y lo dividimos entre 2 para colocarlo a la mitad*/
+        var x = canvas.width / 2;
+        /* Marcamos la posición del texto en x y en y */
+        ctx.fillText(data.usu_nom + ' '+ data.usu_apep +' ' +data.usu_apem, x, 260);
+
+        ctx.font = 'bold 26px Arial';
+        ctx.fillText(data.cur_nom, x, 320);
+
+        ctx.font = 'bold 18px Arial';
+        ctx.fillText(data.inst_nom + ' '+ data.inst_apep +' ' +data.inst_apem, x, 380);
+
+        ctx.font = 'bold 15px Arial';
+        ctx.fillText("99-99-9999", x, 420);
+
     });
-
-    /* Otorgamos los datos necesarios para que pueda imprimir la imagen. */
-
 
 });
 
